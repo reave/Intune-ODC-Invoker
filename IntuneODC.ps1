c026c35623fd608d54b8e0c15d3d036fcc53c80b
Joseph <joseph.ascanio@outlook.com>
2025-04-02 17:36:37 -0400

    Updated Foreach loop for function sourcing to no longer be inline through pipeline.

--- IntuneODC.ps1
+++ IntuneODC.ps1
@@ -10,12 +10,15 @@ if (Test-Path -LiteralPath 'variable:HostInvocation') { $InvocationInfo = $HostI
 $scriptPath = Split-Path -Path $InvocationInfo.MyCommand.Definition -Parent
 
 #- Get all functions from the path of the script\assets\functions and import them with a try catch
-$functions = Get-ChildItem -Path "$scriptPath\assets\functions" -Filter "*.ps1" | ForEach-Object {
+$functions = Get-ChildItem -Path "$scriptPath\assets\functions" -Filter "*.ps1"
+
+ForEach ($function in $functions) {
+    Write-Verbose "Importing function from $($function.FullName)..."
     try {
-        . $_.FullName
+        . $function.FullName
     }
     catch {
-        Write-Error "Failed to import function from $($_.FullName). Error: $_"
+        Write-Error "Failed to import function from $($function.FullName). Error: $_"
     }
 }
 
